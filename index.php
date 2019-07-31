2057bbe07689d81bfe671675243044d2403a4ee8
Ronah143 <53499750+Ronah143@users.noreply.github.com>
2019-07-31 09:40:27 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,22 @@
-<!DOCTYPE.html>
+<!DOCTYPE html>
 <html>
-<head>
-<title>index.html</title>
-</head>
-
+<title>HTML Tutorial</title>
 <body>
 
-<h1>Hello World!</h1>
+<article>
+  <header>
+    <h1>Ronah Jay Q. Emperio</h1>
+    <p>BSIT-1</p>
+  </header>
+</article>
 
+<main>
+  <h1>Address</h1>
+  <p>Tapon Poblacion 3, Carcar City, Cebu</p>
+  
+  <footer>
+  <p>Posted by: Ronah Jay Q. Emperio</p>
+  <p>Contact information: <a href="mailto:ronahjay54@gmail.com">ronahjay54@gmail.com</a>.</p>
+</footer>
 </body>
 </html>
