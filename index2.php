a69d41abbcec30dfe2050d85e08f168fb86e36ce
Ronah143 <53499750+Ronah143@users.noreply.github.com>
2019-07-31 09:37:15 +0800

    Rename index.php to index2.php

---
+++ index2.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<title>HTML Tutorial</title>
+<body>
+
+<article>
+  <header>
+    <h1>Ronah Jay Q. Emperio</h1>
+    <p>BSIT-1</p>
+  </header>
+</article>
+
+<main>
+  <h1>Address</h1>
+  <p>Tapon Poblacion 3, Carcar City, Cebu</p>
+  
+  <footer>
+  <p>Posted by: Ronah Jay Q. Emperio</p>
+  <p>Contact information: <a href="mailto:ronahjay54@gmail.com">ronahjay54@gmail.com</a>.</p>
+</footer>
+
+
+</body>
+</html>
