cdfcbad6bd1ba32c65180f7ecbcbd2368eb8856c
Ronah143 <53499750+Ronah143@users.noreply.github.com>
2019-08-12 12:48:12 +0800

    Add files via upload

---
+++ index1.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>HOME</title>
+
+<Style type='text/css' >body{background-color:rgb(0,0,0);background-image:url(http://www.zingerbugimages.com/backgrounds/brown_ornate_floral_wallpaper_tileable.jpg);background-position:top left;background-repeat:repeat;background-attachment:fixed;}</Style>
+
+</head>
+<body>
+
+ <header>
+    <h1><center><font size= "20">Emperio, RJ's Photo Gallery</font></center></h1>
+  </header>
+
+<center><a title='Image by CommentNation.com' href='http://www.zingerbugimages.com'><img border='0' src='http://www.comments.zingerbugimages.com/glitter_graphics/all_about_me.gif'></a><br><a href='http://www.commentnation.com' width="700" height="600"></a></center>
+
+<h2><center><font size="10">List of Photos</font></center></h2>
+
+<nav><center><font size= "5">
+<a href="file:///C:/Users/EL-EL/Desktop/Jay%20Jay's%20Files/HTML/New/Photo%201.html">Photo 1</a>|
+<a href="file:///C:/Users/EL-EL/Desktop/Jay%20Jay's%20Files/HTML/New/photo%202.html">Photo 2</a>|
+<a href="file:///C:/Users/EL-EL/Desktop/Jay%20Jay's%20Files/HTML/New/Photo%203.html">Photo 3</a>|
+</nav></center></font>
+
+</body>
+</html>
