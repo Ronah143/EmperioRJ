732fe9eac973465d7c955b323660b12f8014a717
Ronah143 <53499750+Ronah143@users.noreply.github.com>
2019-08-12 12:38:49 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,21 +1,26 @@
 <!DOCTYPE html>
-<html lang="en">
-<title>HTML Tutorial</title>
-<body>
-<article>
-  <header>
-    <h1>Ronah Jay Q. Emperio</h1>
-    <p>BSIT-1</p>
-  </header>
-</article>
+<html>
+<head>
+<title>HOME</title>
 
-<main>
-  <h1>Address</h1>
-  <p>Tapon Poblacion 3, Carcar City, Cebu</p>
-  
-  <footer>
-  <p>Posted by: Ronah Jay Q. Emperio</p>
-  <p>Contact information: <a href="mailto:ronahjay54@gmail.com">ronahjay54@gmail.com</a>.</p>
-</footer>
+<Style type='text/css' >body{background-color:rgb(0,0,0);background-image:url(http://www.zingerbugimages.com/backgrounds/brown_ornate_floral_wallpaper_tileable.jpg);background-position:top left;background-repeat:repeat;background-attachment:fixed;}</Style>
+
+</head>
+<body>
+
+ <header>
+    <h1><center><font size= "20">Emperio, RJ's Photo Gallery</font></center></h1>
+  </header>
+
+<center><a title='Image by CommentNation.com' href='http://www.zingerbugimages.com'><img border='0' src='http://www.comments.zingerbugimages.com/glitter_graphics/all_about_me.gif'></a><br><a href='http://www.commentnation.com' width="700" height="600"></a></center>
+
+<h2><center><font size="10">List of Photos</font></center></h2>
+
+<nav><center><font size= "5">
+<a href="file:///C:/Users/EL-EL/Desktop/Jay%20Jay's%20Files/HTML/New/Photo%201.html">Photo 1</a>|
+<a href="file:///C:/Users/EL-EL/Desktop/Jay%20Jay's%20Files/HTML/New/photo%202.html">Photo 2</a>|
+<a href="file:///C:/Users/EL-EL/Desktop/Jay%20Jay's%20Files/HTML/New/Photo%203.html">Photo 3</a>|
+</nav></center></font>
+
 </body>
 </html>
